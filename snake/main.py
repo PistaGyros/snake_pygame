ada76189478a9f6468b6d2870449d17d0c016604
PistaGyros <tomko.jurcik@gmail.com>
2024-04-22 18:43:56 +0200

    Added grid
    Added grid

--- snake/main.py
+++ snake/main.py
@@ -3,85 +3,81 @@ import random
 
 pygame.init()
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
-frameRate = 60
+frameRate = 5
+block_size = 50
+
 
 
 # player
-player = pygame.Rect((275, 275, 50, 50))
-isPaused = False
-segments = []
+class Player():
+    def __init__(self):
+        self.x, self.y = 50, 50
+        self.xdir = 1
+        self.ydir = 0
+        self.head = pygame.Rect(self.x, self.y, 50, 50)
+        self.body = [pygame.Rect(self.x - 50, self.y, 50, 50)]
 
-# food
-blank_object = pygame.Rect(0, 0, 0, 0)
-foods = [blank_object]
-foodColour = (0, 255, 0)
-segmentColour = (0, 0, 255)
+    def update(self):
+        self.body.append(self.head)
+        for i in range(len(self.body)-1):
+            self.body[i].x, self.body[i].y = self.body[i + 1].x, self.body[i + 1].y
+        self.head.x += self.xdir * block_size
+        self.head.y += self.ydir * block_size
+        self.body.remove(self.head)
+
+def DrawGrid():
+    for x in range(0, SCREEN_WIDTH, block_size):
+        for y in range(0, SCREEN_HEIGHT, block_size):
+            rect = pygame.Rect(x, y, block_size, block_size)
+            pygame.draw.rect(screen, "#3c3c3b", rect, 1)
+
+DrawGrid()
 
 horizontalDirection = 0
 verticalDirection = 1
 horizontalDirectionHasBeenChanged = False
 verticalDirectionHasBeenChanged = False
 
+player = Player()
+
 run = True
 
-
-def spawnFood():
-    randomX = random.randint(1, 550)
-    randomY = random.randint(1, 550)
-    food_prefab = pygame.Rect(randomX, randomY, 50, 50)
-    foods.append(food_prefab)
-
-def addSegment():
-
-
-
-spawnFood()
-
 while run:
-    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, "green", player.head)
 
-    pygame.draw.rect(screen, (255, 0, 0), player)
+    player.update()
+    screen.fill("black")
+    DrawGrid()
 
-    for food in foods:
-        pygame.draw.rect(screen, foodColour, food)
+    for square in player.body:
+        pygame.draw.rect(screen, "green", square)
 
     key = pygame.key.get_pressed()
-    if key[pygame.K_a] and not horizontalDirectionHasBeenChanged:
-        horizontalDirection = -1
-        verticalDirection = 0
+    if key[pygame.K_a] or key[pygame.K_LEFT] and horizontalDirectionHasBeenChanged:
+        player.xdir = -1
+        player.ydir = 0
         horizontalDirectionHasBeenChanged = True
         verticalDirectionHasBeenChanged = False
-    elif key[pygame.K_d] and not horizontalDirectionHasBeenChanged:
-        horizontalDirection = 1
-        verticalDirection = 0
+    elif key[pygame.K_d] or key[pygame.K_RIGHT] and horizontalDirectionHasBeenChanged:
+        player.xdir = 1
+        player.ydir = 0
         horizontalDirectionHasBeenChanged = True
         verticalDirectionHasBeenChanged = False
-    elif key[pygame.K_w] and not verticalDirectionHasBeenChanged:
-        verticalDirection = -1
-        horizontalDirection = 0
+    elif key[pygame.K_w] or key[pygame.K_UP] and verticalDirectionHasBeenChanged:
+        player.xdir = 0
+        player.ydir = -1
         horizontalDirectionHasBeenChanged = False
         verticalDirectionHasBeenChanged = True
-    elif key[pygame.K_s] and not verticalDirectionHasBeenChanged:
-        verticalDirection = 1
-        horizontalDirection = 0
+    elif key[pygame.K_s] or key[pygame.K_DOWN] and verticalDirectionHasBeenChanged:
+        player.xdir = 0
+        player.ydir = 1
         horizontalDirectionHasBeenChanged = False
         verticalDirectionHasBeenChanged = True
 
-    player.move_ip(5 * horizontalDirection, 5 * verticalDirection)
-
-    # collision detection
-    if player.y == 0 or player.y == 550 or player.x == 0 or player.x == 600:
-        run = False
-
-    for food in foods:
-        if player.colliderect(food):
-            foods.remove(food)
-            addSegment()
-            spawnFood()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
